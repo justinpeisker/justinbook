8ed0eb1264bc0e172d7215bbefc72aab2a16f326
Justin Peisker <justinpeisker@gmail.com>
2021-10-23 08:30:14 -0700

    users component scaffolded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const admin = {
-
+  location: 'California',
+  uuid: '12345',
+  picture: 'https://avatars.githubusercontent.com/u/87906218?v=4',
+  name: 'Justin Peisker',
+  email: 'j@j.com'
 }
+
+
+
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=5')
+    .then(res => {
+      // console.log(res);
+      setUsers(res.data.results);
+    }).catch(err => {
+      console.error(err);
+    })
+  }, [])
+
   return (
     <div className="App">
      <h1>JustinBook</h1>
---
+++ src/Components/Users.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Users = (props) => {
+
+    return(
+        <h2>Users</h2>
+    )
+}
+
+export default Users;
